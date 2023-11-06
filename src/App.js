31378eed7df924a4531472cf050048b1909afaea
MBUYT0N <shusrith03@gmail.com>
2023-11-06 15:01:31 +0530

    gooooooooooooooooooood

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const { MongoClient } = require("mongodb");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const uri =
+	"mongodb+srv://pes1202201377:lisanlisan@cluster0.buvuxr1.mongodb.net/?retryWrites=true&w=majority";
 
-export default App;
+const HomePage = () => {
+	const [results, setResults] = useState();
+	const client = new MongoClient(uri)
+		async function fetchData() {
+			try {
+				await client.connect(); // Connect to the MongoDB server
+				const database = client.db("wwt-project");
+				const movies = database.collection("recipe sharing");
+
+				const result = await movies
+					.aggregate([{ $sample: { size: 1 } }])
+					.toArray();
+				setResults(result);
+			} finally {
+				// Ensures that the client will close when you finish/error
+				await client.close();
+			}
+		}
+
+		fetchData().catch(console.error);
+	; // Empty dependency array to ensure this effect runs only once on component mount
+
+	return (
+		<div>
+
+				<div key={results._id}>
+					<h1>{results.title}</h1>
+					<h2>{results.director}</h2>
+					<h3>{results.year}</h3>
+				</div>
+		</div>
+	);
+};
+
+export default HomePage;
+
--- src/a.js
+++ src/a.js
@@ -1,45 +1,93 @@
+// // const { MongoClient } = require("mongodb");
+
+// // // Replace the uri string with your connection string.
+// // const uri =
+// // 	"mongodb+srv://pes1202201377:lisanlisan@cluster0.buvuxr1.mongodb.net/?retryWrites=true&w=majority";
+
+// // const client = new MongoClient(uri);
+
+// // async function run() {
+// // 	try {
+// // 		const database = client.db("wwt-project");
+// // 		const movies = database.collection("recipe sharing");
+
+// // 		const documents = [
+// // 			{
+// // 				title: "Inception",
+// // 				director: "Christopher Nolan",
+// // 				year: 2010,
+// // 			},
+// // 			{
+// // 				title: "nowtakwb",
+// // 				director: "me",
+// // 				year: 30102,
+// // 			},
+// // 			{
+// // 				title: "oweffjfw",
+// // 				director: "not me",
+// // 				year: 4232,
+// // 			},
+// // 			{
+// // 				title: "thanks ",
+// // 				director: "him",
+// // 				year: 433,
+// // 			},
+// // 		];
+
+// // 		const result = await movies.insertMany(documents);
+
+// // 		console.log(`Inserted ${result.insertedCount} documents`);
+// // 	} finally {
+// // 		// Ensures that the client will close when you finish/error
+// // 		await client.close();
+// // 	}
+// // }
+// // run().catch(console.dir);
+
+// // import React, { useState } from "react";
+// const { MongoClient } = require("mongodb");
+
+// const uri =
+// 	"mongodb+srv://pes1202201377:iamdarock2004@cluster0.buvuxr1.mongodb.net/?retryWrites=true&w=majority";
+
+// const client = new MongoClient(uri);
+
+// async function run() {
+// 	try {
+// 		const database = client.db("wwt-project");
+// 		const movies = database.collection("recipe sharing");
+
+// 		movies.aggregate([{ Sample: { size: 4 } }]).toArray((err, result) => {
+// 			if (err) throw err;
+// 			items = [result];
+// 			console.log(result);
+// 		});
+// 	} finally {
+// 		// Ensures that the client will close when you finish/error
+// 		await client.close();
+// 	}
+// }
+// run().catch(console.dir);
+
 const { MongoClient } = require("mongodb");
 
-// Replace the uri string with your connection string.
 const uri =
-	"mongodb+srv://pes1202201377:iamdarock2004@cluster0.buvuxr1.mongodb.net/?retryWrites=true&w=majority";
+	"mongodb+srv://pes1202201377:lisanlisan@cluster0.buvuxr1.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
 async function run() {
 	try {
+		await client.connect(); // Connect to the MongoDB server
 		const database = client.db("wwt-project");
 		const movies = database.collection("recipe sharing");
 
-		const documents = [
-			{
-				title: "Inception",
-				director: "Christopher Nolan",
-				year: 2010,
-			},
-			{
-				title: "nowtakwb",
-				director: "me",
-				year: 30102,
-			},
-			{
-				title: "oweffjfw",
-				director: "not me",
-				year: 4232,
-			},
-			{
-				title: "thanks ",
-				director: "him",
-				year: 433,
-			},
-		];
-
-		const result = await movies.insertMany(documents);
-
-		console.log(`Inserted ${result.insertedCount} documents`);
+		const result = await movies.aggregate([{ $sample: { size: 4 } }]).toArray();
+		console.log(result)
 	} finally {
 		// Ensures that the client will close when you finish/error
 		await client.close();
 	}
 }
+
 run().catch(console.dir);
