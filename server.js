2ca46053a841990a10d543e1526e4a22373e9758
MBUYT0N <shusrith03@gmail.com>
2023-11-10 12:39:19 +0530

    changes chaanges changes

--- server.js
+++ server.js
@@ -4,57 +4,64 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 mongoose.connect(
-	"mongodb+srv://pes1202201377:lisanlisan@cluster0.buvuxr1.mongodb.net/wwt-project?retryWrites=true&w=majority",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	}
+  "mongodb+srv://pes1202201377:lisanlisan@cluster0.buvuxr1.mongodb.net/wwt-project?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
-
 app.get("/api/recipe", async (req, res) => {
-	try {
-		const randomRecipes = await mongoose.connection.db
-			.collection("recipe-sharing")
-			.find({})
-			.toArray();
+  try {
+    const hello123Recipes = await getRecipes("hello123");
+    const goodmorningRecipes = await getRecipes("goodmornings");
+    const yellosRecipes = await getRecipes("yellos");
+    const lisansRecipes = await getRecipes("lisans");
 
-		const hello123Recipes = await mongoose.connection.db
-			.collection("hello123")
-			.find({})
-			.toArray();
+    const allRecipes = [
+      ...hello123Recipes,
+      ...goodmorningRecipes,
+      ...yellosRecipes,
+      ...lisansRecipes,
+    ];
 
-		const goodmorningRecipes = await mongoose.connection.db
-			.collection("goodmornings")
-			.find({})
-			.toArray();
-
-		const yellosRecipes = await mongoose.connection.db
-			.collection("yellos")
-			.find({})
-			.toArray();
-
-		const lisansRecipes = await mongoose.connection.db
-			.collection("lisans")
-			.find({})
-			.toArray();
-
-		const allRecipes = [
-			...randomRecipes,
-			...hello123Recipes,
-			...goodmorningRecipes,
-			...yellosRecipes,
-			...lisansRecipes,
-		];
-
-		res.json(allRecipes);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: "Internal Server Error" });
-	}
+    res.json(allRecipes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
+app.post("/api/recipe/like/:id/:collectionName", async (req, res) => {
+  const recipeId = req.params.id;
+  const collectionName = req.params.collectionName;
+
+  try {
+    await updateLikes(collectionName, recipeId);
+
+    res.json({ message: "Likes updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+async function getRecipes(collectionName) {
+  return await mongoose.connection.db.collection(collectionName).find({}).toArray();
+}
+
+async function updateLikes(collectionName, recipeId) {
+  await mongoose.connection.db
+    .collection(collectionName)
+    .updateOne(
+      { _id: mongoose.Types.ObjectId(recipeId) },
+      { $inc: { likes: 1 } }
+    );
+}
+
 app.listen(5000, () => {
-	console.log("Server is running on http://localhost:5000");
+  console.log("Server is running on http://localhost:5000");
 });
--- src/randomrecipes.js
+++ src/randomrecipes.js
@@ -1,13 +1,186 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const renderRecipeBody = (body) => {
-	return body.split("\n").map((line, i) => (
-		<React.Fragment key={i}>
-			{i > 0 && <br />} {/* Add line break if not the first line */}
-			{line}
-		</React.Fragment>
-	));
-};
+// const RandomRecipes = () => {
+// 	const [randomRecipes, setRandomRecipes] = useState([]);
+// 	const [loading, setLoading] = useState(false);
+
+// 	useEffect(() => {
+// 		const loadMoreRecipes = () => {
+// 			if (!loading) {
+// 				setLoading(true);
+
+// 				fetch("http://localhost:5000/api/recipe")
+// 					.then((response) => response.json())
+// 					.then((data) => {
+// 						setRandomRecipes((prevRecipes) => [
+// 							...prevRecipes,
+// 							...data,
+// 						]);
+// 						setLoading(false);
+// 					})
+// 					.catch((error) => {
+// 						console.error("Error:", error);
+// 						setLoading(false);
+// 					});
+// 			}
+// 		};
+
+// 		const observer = new IntersectionObserver(
+// 			(entries) => {
+// 				if (entries[0].isIntersecting) {
+// 					loadMoreRecipes();
+// 				}
+// 			},
+// 			{ threshold: 0.1 }
+// 		);
+
+// 		observer.observe(document.getElementById("load-more-trigger"));
+
+// 		return () => {
+// 			observer.disconnect();
+// 		};
+// 	}, [loading]);
+
+// 	const handleExpand = (index) => {
+// 		const updatedRecipes = randomRecipes.map((recipe, i) =>
+// 			i === index
+// 				? { ...recipe, expanded: !recipe.expanded }
+// 				: { ...recipe, expanded: false }
+// 		);
+// 		setRandomRecipes(updatedRecipes);
+// 	};
+
+// 	const handleLike = async (index, collectionName) => {
+// 		try {
+// 			const response = await fetch(
+// 				`http://localhost:5000/api/recipe/like/${randomRecipes[index]._id}/${collectionName}`,
+// 				{
+// 					method: "POST",
+// 					headers: {
+// 						"Content-Type": "application/json",
+// 					},
+// 				}
+// 			);
+
+// 			if (response.ok) {
+// 				const updatedRecipes = [...randomRecipes];
+// 				updatedRecipes[index].like += 1;
+// 				setRandomRecipes(updatedRecipes);
+// 			} else {
+// 				console.error("Failed to update likes");
+// 			}
+// 		} catch (error) {
+// 			console.error("Error:", error);
+// 		}
+// 	};
+
+// 	const styles = {
+// 		container: {
+// 			fontFamily: "Arial, sans-serif",
+// 			backgroundColor: "#FF0000", // Red
+// 			padding: "20px",
+// 		},
+// 		content: {
+// 			display: "flex",
+// 			border: "2px solid #ccc",
+// 			padding: "10px",
+// 			margin: "10px",
+// 			background: "#FFFF00", // Yellow
+// 			boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+// 			overflow: "hidden",
+// 			textOverflow: "ellipsis",
+// 			whiteSpace: "nowrap",
+// 			cursor: "pointer",
+// 		},
+// 		leftContent: {
+// 			display: "flex",
+// 			flexDirection: "column",
+// 			marginRight: "10px", // Adjust as needed for spacing
+// 		},
+// 		rightContent: {
+// 			flex: 1,
+// 		},
+// 		expanded: {
+// 			transform: "scale(1.05)",
+// 		},
+// 		title: {
+// 			fontSize: "24px",
+// 			marginBottom: "10px",
+// 			color: "#333",
+// 		},
+// 		smallerText: {
+// 			fontSize: "16px",
+// 			color: "#555",
+// 			whiteSpace: "pre-line",
+// 		},
+
+// 		likeCount: {
+// 			fontSize: "14px",
+// 			color: "#333",
+// 			textAlign: "center",
+// 		},
+
+// 		likeIcon: {
+// 			fontSize: "24px",
+// 			color: "#FF0000",
+// 			cursor: "pointer",
+// 		},
+// 	};
+
+// 	const renderRecipeBody = (body) => {
+// 		return (
+// 			<div
+// 				style={{ ...styles.smallerText, whiteSpace: "pre-line" }}
+// 				dangerouslySetInnerHTML={{ __html: body }}
+// 			/>
+// 		);
+// 	};
+
+// 	return (
+// 		<div style={styles.container}>
+// 			<h1 style={{ textAlign: "center", color: "#333" }}>
+// 				Random Recipes
+// 			</h1>
+
+// 			{randomRecipes.map((recipe, index) => (
+// 				<div
+// 					key={recipe._id}
+// 					id="content"
+// 					style={{
+// 						...styles.content,
+// 						...(recipe.expanded && styles.expanded),
+// 					}}
+// 					onClick={() => handleExpand(index)}
+// 				>
+// 					<div style={styles.leftContent}>
+// 						<span
+// 							onClick={() =>
+// 								handleLike(index, recipe.collectionName)
+// 							}
+// 							style={styles.likeIcon}
+// 						>
+// 							❤️
+// 						</span>
+// 						<div style={styles.likeCount}>{recipe.like}</div>
+// 					</div>
+// 					<div style={styles.rightContent}>
+// 						<div style={styles.title}>{recipe.title}</div>
+// 						<div style={styles.smallerText}>
+// 							{renderRecipeBody(recipe.body)}
+// 						</div>
+// 						<div style={styles.smallerText}>{recipe.year}</div>
+// 					</div>
+// 				</div>
+// 			))}
+
+// 			<div id="load-more-trigger" style={{ height: "1px" }} />
+// 		</div>
+// 	);
+// };
+
+// export default RandomRecipes;
+
+import React, { useState, useEffect } from "react";
 
 const RandomRecipes = () => {
 	const [randomRecipes, setRandomRecipes] = useState([]);
@@ -50,6 +223,39 @@ const RandomRecipes = () => {
 		};
 	}, [loading]);
 
+	const handleExpand = (index) => {
+		const updatedRecipes = randomRecipes.map((recipe, i) =>
+			i === index
+				? { ...recipe, expanded: !recipe.expanded }
+				: { ...recipe, expanded: false }
+		);
+		setRandomRecipes(updatedRecipes);
+	};
+
+	const handleLike = async (index, collectionName) => {
+		try {
+			const response = await fetch(
+				`http://localhost:5000/api/recipe/like/${randomRecipes[index]._id}/${collectionName}`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			if (response.ok) {
+				const updatedRecipes = [...randomRecipes];
+				updatedRecipes[index].like += 1;
+				setRandomRecipes(updatedRecipes);
+			} else {
+				console.error("Failed to update likes");
+			}
+		} catch (error) {
+			console.error("Error:", error);
+		}
+	};
+
 	const styles = {
 		container: {
 			fontFamily: "Arial, sans-serif",
@@ -57,21 +263,31 @@ const RandomRecipes = () => {
 			padding: "20px",
 		},
 		content: {
+			display: "flex",
 			border: "2px solid #ccc",
 			padding: "10px",
 			margin: "10px",
-			textAlign: "center",
-			transition: "transform 0.3s",
-			cursor: "pointer",
-			maxWidth: "600px",
-			marginLeft: "auto",
-			marginRight: "auto",
 			background: "#FFFF00", // Yellow
 			boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+			overflow: "hidden",
+			cursor: "pointer",
+			textOverflow: "ellipsis",
+			whiteSpace: "nowrap",
+			maxHeight: "50px", // Set a fixed height for collapsed state
+			transition: "max-height 0.3s ease-in-out",
+			maxWidth: "66.666%", // Set to 2/3rds of the screen width
 		},
 		expanded: {
 			transform: "scale(1.05)",
 			maxHeight: "none",
+		},
+		leftContent: {
+			display: "flex",
+			flexDirection: "column",
+			marginRight: "10px", // Adjust as needed for spacing
+		},
+		rightContent: {
+			flex: 1,
 		},
 		title: {
 			fontSize: "24px",
@@ -81,17 +297,27 @@ const RandomRecipes = () => {
 		smallerText: {
 			fontSize: "16px",
 			color: "#555",
-			// whiteSpace: "pre-line", // Add this line for preserving line breaks
+			whiteSpace: "pre-line",
+		},
+		likeCount: {
+			fontSize: "14px",
+			color: "#333",
+			textAlign: "center",
+		},
+		likeIcon: {
+			fontSize: "24px",
+			color: "#FF0000",
+			cursor: "pointer",
 		},
 	};
 
-	const handleExpand = (index) => {
-		const updatedRecipes = randomRecipes.map((recipe, i) =>
-			i === index
-				? { ...recipe, expanded: !recipe.expanded }
-				: { ...recipe, expanded: false }
+	const renderRecipeBody = (body) => {
+		return (
+			<div
+				style={{ ...styles.smallerText, whiteSpace: "pre-line" }}
+				dangerouslySetInnerHTML={{ __html: body }}
+			/>
 		);
-		setRandomRecipes(updatedRecipes);
 	};
 
 	return (
@@ -110,11 +336,24 @@ const RandomRecipes = () => {
 					}}
 					onClick={() => handleExpand(index)}
 				>
-					<div style={styles.title}>{recipe.title}</div>
-					<div style={styles.smallerText}>
-						{renderRecipeBody(recipe.body)}
+					<div style={styles.leftContent}>
+						<span
+							onClick={() =>
+								handleLike(index, recipe.collectionName)
+							}
+							style={styles.likeIcon}
+						>
+							❤️
+						</span>
+						<div style={styles.likeCount}>{recipe.like}</div>
 					</div>
-					<div style={styles.smallerText}>{recipe.year}</div>
+					<div style={styles.rightContent}>
+						<div style={styles.title}>{recipe.title}</div>
+						<div style={styles.smallerText}>
+							{renderRecipeBody(recipe.body)}
+						</div>
+						<div style={styles.smallerText}>{recipe.year}</div>
+					</div>
 				</div>
 			))}
 
