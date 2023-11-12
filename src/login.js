cc5d83d555161fa67b6bdbeaf13ccb861b17ccba
MBUYt0n <shusrith03@gmail.com>
2023-11-12 19:21:36 +0530

    yes

--- src/login.js
+++ src/login.js
@@ -27,13 +27,40 @@ const Login = ({ onLoginSuccess }) => {
 		}
 	};
 
+	const containerStyle = {
+		maxWidth: "400px",
+		margin: "0 auto",
+		backgroundColor: "rgba(255, 255, 255, 0.8)",
+		padding: "20px",
+		borderRadius: "8px",
+		boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+		color: "black",
+	};
+
+	const inputStyle = {
+		width: "100%",
+		padding: "10px",
+		margin: "8px 0",
+		boxSizing: "border-box",
+	};
+
+	const buttonStyle = {
+		backgroundColor: "#fff",
+		color: "#333",
+		padding: "12px 20px",
+		border: "none",
+		borderRadius: "4px",
+		cursor: "pointer",
+	};
+
 	return (
-		<div>
+		<div style={containerStyle}>
 			<h2>Login</h2>
 			<form>
 				<label>
 					Username:
 					<input
+						style={inputStyle}
 						type="text"
 						value={username}
 						onChange={(e) => setUsername(e.target.value)}
@@ -43,13 +70,14 @@ const Login = ({ onLoginSuccess }) => {
 				<label>
 					Password:
 					<input
+						style={inputStyle}
 						type="password"
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
 				</label>
 				<br />
-				<button type="button" onClick={handleLogin}>
+				<button type="button" style={buttonStyle} onClick={handleLogin}>
 					Login
 				</button>
 			</form>
