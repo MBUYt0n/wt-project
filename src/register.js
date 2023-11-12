c6b14b8a0409d11330e070475029481531181c39
sidx-4 <149075822+sidx-4@users.noreply.github.com>
2023-11-12 22:51:29 +0530

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,76 +1,141 @@
 import React, { useState } from "react";
 
 const RegisterPage = ({ onRegisterSuccess }) => {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const handleRegister = async () => {
-		try {
-			const response = await fetch("http://localhost:5000/api/register", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					username: username,
-					password: password,
-				}),
-			});
+  const handleRegister = async (e) => {
+    e.preventDefault();
 
-			if (response.ok) {
-				console.log("Registration successful!");
-                onRegisterSuccess(username, password)
-				// You can redirect the user to the login page or perform other actions after successful registration
-			} else {
-				console.error("Registration failed:", response.statusText);
-			}
-		} catch (error) {
-			console.error("Registration failed:", error.message);
-		}
-	};
+    try {
+      const response = await fetch("http://localhost:5000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
 
-	return (
-		<div className="container">
-			<h2>Register</h2>
-			<form>
-				<label>Username:</label>
-				<input
-					type="text"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-					required
-				/>
+      if (response.ok) {
+        console.log("Registration successful!");
+        onRegisterSuccess(username, password);
+        // You can redirect the user to the login page or perform other actions after successful registration
+      } else {
+        console.error("Registration failed:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Registration failed:", error.message);
+    }
+  };
 
-				<label>Email:</label>
-				<input
-					type="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					required
-				/>
+  return (
+    <div className="container">
+      <style>
+        {`
+          body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background: linear-gradient(to right, #667eea, #764ba2);
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+          }
 
-				<label>Password:</label>
-				<input
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					required
-				/>
+          form {
+            background: linear-gradient(to right, #fff, #f5f5f5);
+            padding: 30px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 100%;
+            max-width: 400px;
+            text-align: center;
+          }
 
-				<button type="button" onClick={handleRegister}>
-					Register
-				</button>
-			</form>
+          h2 {
+            color: #333;
+          }
 
-			<p>
-				Already have an account?{" "}
-				<a href="login.js" className="login-link">
-					Login here!
-				</a>
-			</p>
-		</div>
-	);
+          label {
+            display: block;
+            text-align: left;
+            margin-top: 10px;
+          }
+
+          input, select {
+            width: calc(100% - 16px);
+            padding: 10px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+          }
+
+          button {
+            background: linear-gradient(to right, #45a049, #4caf50);
+            color: #fff;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+          }
+
+          button:hover {
+            background: linear-gradient(to right, #4caf50, #45a049);
+          }
+
+          a {
+            color: #333;
+          }
+
+          a:hover {
+            color: #000;
+          }
+        `}
+      </style>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Register</button>
+      </form>
+
+      <p>
+        Already have an account?{" "}
+        <a href="login.js" className="login-link">
+          Login here!
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default RegisterPage;
