65a2230a8cada7015c9ba87110f8843f1eec7d03
sidx-4 <149075822+sidx-4@users.noreply.github.com>
2023-11-16 12:54:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
 import React, { useState } from "react";
-import RandomRecipes from "./randomrecipes";
 import Login from "./login";
 import RegisterPage from "./register";
-import ProfilePage from "./profile"; 
+import ProfilePage from "./profile";
 
 const App = () => {
-	const [isLoggedIn, setLoggedIn] = useState(false);
-	const [credentials, setCredentials] = useState({});
-	const [showRegister, setShowRegister] = useState(false);
+  // State to manage authentication status and credentials
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [credentials, setCredentials] = useState({});
+  const [showRegister, setShowRegister] = useState(false);
 
-	const handleLoginSuccess = (username, password) => {
-		setLoggedIn(true);
-		setCredentials({ username, password });
-	};
+  // Callback for successful login
+  const handleLoginSuccess = (username, password) => {
+    setLoggedIn(true);
+    setCredentials({ username, password });
+  };
 
-	const handleRegisterSuccess = (username, password) => {
-		console.log("Registration successful!");
-		setLoggedIn(true);
-		setCredentials({ username, password });
-		setShowRegister(false);
-	};
+  // Callback for successful registration
+  const handleRegisterSuccess = (username, password) => {
+    console.log("Registration successful!");
+    setLoggedIn(true);
+    setCredentials({ username, password });
+    setShowRegister(false);
+  };
 
-	const handleLogout = () => {
-		setLoggedIn(false);
-		setCredentials({ username: "", password: "" });
-	};
+  // Callback for logout
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setCredentials({ username: "", password: "" });
+  };
 
-	return (
-		<div>
-			{isLoggedIn ? (
-				<ProfilePage
-					credentials={credentials}
-					handleLogout={handleLogout}
-				/>
-			) : (
-				<div>
-					{showRegister ? (
-						<RegisterPage
-							onRegisterSuccess={handleRegisterSuccess}
-							setShowRegister={setShowRegister}
-						/>
-					) : (
-						<Login
-							onLoginSuccess={handleLoginSuccess}
-							setShowRegister={setShowRegister}
-						/>
-					)}
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {/* Check authentication status */}
+      {isLoggedIn ? (
+        // If logged in, render the ProfilePage
+        <ProfilePage credentials={credentials} handleLogout={handleLogout} />
+      ) : (
+        // If not logged in, render either Login or RegisterPage based on showRegister state
+        <div>
+          {showRegister ? (
+            <RegisterPage
+              onRegisterSuccess={handleRegisterSuccess}
+              setShowRegister={setShowRegister}
+            />
+          ) : (
+            <Login
+              onLoginSuccess={handleLoginSuccess}
+              setShowRegister={setShowRegister}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
